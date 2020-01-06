f5419d2b11c9ec2f4d0a3322b45dc0eb282edd47
bill41708 <33356634+bill41708@users.noreply.github.com>
2020-01-06 19:31:36 +0800

    Add files via upload

--- DHTtester.ino
+++ DHTtester.ino
@@ -5,7 +5,9 @@
 #include <LoRa.h>
 
 #define DHTPIN 2     // what pin we're connected to
-#define relay_pin 13
+#define relay_pin 12
+#define SensorPin 27
+#define inPin  21
 
 // Uncomment whatever type you're using!
 //#define DHTTYPE DHT11   // DHT 11 
@@ -15,10 +17,13 @@
 /*Notice: The DHT10 is different from other DHT* sensor ,it uses i2c interface rather than one wire*/
           /*So it doesn't require a pin.*/
 
-char currentrs[64]; int cnt=0; int num=0; String tmp=""; char currentid[64];
+char currentrs[64]; int cnt=0; int num=0; String tmp=""; char currentid[64]; String tmp1="";
 String payload="";
 String receivedText;
 String receivedRssi;
+float sensorValue = 0; 
+int val = LOW;
+int flag = 0;
 
 
 #define SS      18
@@ -42,6 +47,7 @@ void setup()
     
     Serial.begin(115200); 
     pinMode(relay_pin, OUTPUT);
+    pinMode(inPin, INPUT);
     Serial.println("DHTxx test!");
     Wire.begin();
 
@@ -50,24 +56,22 @@ void setup()
     // digitalWrite(PIN_GROVE_POWER, 1);
 
     dht.begin();
+    
 
     if (!LoRa.begin(BAND)) {
       Serial.println("Starting LoRa failed!");
       //u8x8.drawString(0, 1, "Starting LoRa failed!");
       while (1);
   }
+    
   
 }
 
 void loop() 
 {
+    delay(10);
     float temp_hum_val[2] = {0};
     digitalWrite(relay_pin, LOW);
-    delay(1000);
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    
-    
     if(!dht.readTempAndHumidity(temp_hum_val)){
         Serial.print("Humidity: "); 
         Serial.print(temp_hum_val[0]);
@@ -75,46 +79,82 @@ void loop()
         Serial.print("Temperature: "); 
         Serial.print(temp_hum_val[1]);
         Serial.println(" *C");
+        delay(10);
+        
+        tmp1 = "Humidity: "+ String(temp_hum_val[0]) + " %\t" + "Temperature: "+ String(temp_hum_val[1]) + " *C";
 
 
-        if(int(temp_hum_val[0]) > 90){
+        //if(int(temp_hum_val[0]) > 90){
 
-          
-          payload="Water flower";
-          cnt=(cnt+1) % 10;
-          for(int i=1; i<=3; i++)
-          { LoRa.beginPacket(); 
-            tmp=payload+"_"+String(cnt);
-            LoRa.print(tmp); 
-            Serial.println("LoraSend:"+tmp);
-            LoRa.endPacket(); //retorno= 1:sucesso | 0: falha 
-            
-            int packetSize;
-            for(int i=1; i<=10; i++)
-            {
-                packetSize = LoRa.parsePacket();
-                if (packetSize) 
-                { receivedText="";
-                  while (LoRa.available()) {receivedText=receivedText+(char)LoRa.read(); }
-                  Serial.print(receivedText); break; }
-            }
-            delay(10);
-          }
-          int packetSize=0;
-
-
-
-          digitalWrite(relay_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
-          delay(5000);                       // wait for a second
-          digitalWrite(relay_pin, LOW);    // turn the LED off by making the voltage LOW
-          delay(5000); 
-
-          
-        }
     }
     else{
        Serial.println("Failed to get temprature and humidity value.");
     }
+
+    
+    delay(10);
+    //val = digitalRead(inPin);
+    
+    if (digitalRead(inPin) == LOW)
+    {
+      Serial.println("LOW");
+      flag = 0;
+    }
+    else
+    {
+      Serial.println("HIGH");
+      flag = 1;
+    }
+    
+    /*
+    val = digitalRead(inPin);
+    Serial.println(val);
+    */
+    
+    
+    
+    delay(1000);
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+
+    sensorValue = 0;
+
+    sensorValue = analogRead(SensorPin); 
+    delay(100); 
+
+    //sensorValue = sensorValue/100.0;
+    Serial.print("soil moisture: ");
+    Serial.println(sensorValue); 
+    delay(30);
+    
+    if (flag > 0){
+
+      payload="Water flower";
+      cnt=(cnt+1) % 10;
+      
+      for(int i=1; i<4; i++)
+      { 
+        LoRa.beginPacket(); 
+        
+        tmp=payload+"_"+String(cnt)+" ";
+        LoRa.print(tmp);
+        LoRa.print(tmp1);
+        //LoRa.endPacket(); //retorno= 1:sucesso | 0: falha
+         
+        Serial.println("LoraSend:"+tmp+" "+tmp1);
+        delay(100);
+      }
+      
+
+      digitalWrite(relay_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(5000);                       // wait for a second
+      digitalWrite(relay_pin, LOW);    // turn the LED off by making the voltage LOW
+      delay(100); 
+    }
+
+
+
+
 
    delay(1500);
 }
